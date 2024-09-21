55a83db5d9c7d34e0acac0b7682a6108f4aa36fe
SryIDC <123440389+SryIDC@users.noreply.github.com>
2024-09-21 06:22:08 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,18 @@
 config = {}
 
+config.NotificationType = "ox_lib" -- ox_lib or qbx_core
+
 config.boss = {
     model = "a_m_m_og_boss_01", -- Boss model
     cooldown = 120, -- in seconds
-    blip = false, -- to show boss location in map or not
+    owned_cars = false, --Can deliver player owned vehicles?
+    blip = {
+        enable = true,
+        text = "Illegal Car text",
+        color = 1,
+        sprite = 229,
+        scale = 1.0,
+    }, -- to show boss location in map or not
     coords = {x= 495.6126, y=-1340.8628, z=29.3121, h = 4.3495},
     drop = {x=406.3838, y=2596.4126, z=43.5194, h=12.3785}
 }
@@ -27,28 +36,6 @@ config.Vehicles = { -- List of vehicles only in uppercase
 
 config.Reward = {
     account = "cash", --bank, cash, crypto,
-    reason = "Illegal Car Delivery" -- Bank statement reason idk
-}
-
-config.Rewards = {
-    [0] = 1000, -- Compact
-    [1] = 2000, -- Sedan
-    [2] = 3000, -- SUV
-    [3] = 4000, -- Coupe
-    [4] = 5000, -- Muscle
-    [5] = 6000, -- Sport
-    [6] = 7000, -- Sport Classic
-    [7] = 8000, -- SUV
-    [8] = 9000, -- Off-road
-    [9] = 10000, -- Industrial
-    [10] = 11000, -- Utility
-    [11] = 12000, -- Van
-    [12] = 13000, -- BMX
-    [13] = 14000, -- Boat
-    [14] = 15000, -- Helicopter
-    [15] = 16000, -- Plane
-    [16] = 17000, -- Service
-    [17] = 18000, -- Emergency
-    [18] = 19000, -- Military
-    [19] = 20000, -- Commercial
+    reason = "Illegal Car Delivery", -- Bank statement reason idk
+    amount = math.random(8000, 12000),
 }
