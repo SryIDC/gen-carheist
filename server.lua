b820034fc3affc8248c25fdb3594f7dcc5f85d39
SryIDC <123440389+SryIDC@users.noreply.github.com>
2024-09-21 06:20:42 +0530

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,25 +1,23 @@
 local robberyCooldown = config.boss.cooldown  
 local lastRobberyTimes = {}
 
-RegisterNetEvent("gen-carheist:server:reward")
-AddEventHandler("gen-carheist:server:reward", function (amount)
+RegisterNetEvent("gen-carheist:server:reward", function ()
     local src = source
     local Player = exports.qbx_core:GetPlayer(src)
 
-    Player.Functions.AddMoney(config.Reward.account, amount, config.Reward.reason)
+    Player.Functions.AddMoney(config.Reward.account, config.Reward.amount, config.Reward.reason)
     TriggerClientEvent('ox_lib:notify', src, {
         title = "Genesis",
-        description = "You have delivered the vehicle and was rewarded: $"..amount,
+        description = "You have delivered the vehicle and was rewarded: $"..config.Reward.amount,
         type = "success",
         icon = "fa-solid fa-money-bill",
         duration = 8000
     })
 end)
 
-RegisterNetEvent("gen-carheist:server:cooldown")
-AddEventHandler("gen-carheist:server:cooldown", function()
+RegisterNetEvent("gen-carheist:server:cooldown", function()
     local src = source
-    local currentTime = os.time()
+
 
     if lastRobberyTimes[src] then
         local lastRobberyTime = lastRobberyTimes[src]
@@ -37,4 +35,21 @@ AddEventHandler("gen-carheist:server:cooldown", function()
     TriggerClientEvent("gen-carheist:client:startcarheist", src)
     lastRobberyTimes[src] = currentTime
 
+end)
+
+RegisterServerEvent('gen-carheist:server:sql', function (plate, vehicle)
+    local src = source
+    MySQL.query('SELECT `plate` FROM `player_vehicles` WHERE `plate` = ?', {
+        plate
+    }, function(response)
+        if response and #response > 0  then
+            TriggerClientEvent('ox_lib:notify', src, {
+                title = "Genesis",
+                description = "You can't deliver a player owned vehicle!",
+                type = "error",
+            })
+        else
+            TriggerClientEvent('gen-carheist:client:deliversuccess', src, vehicle)
+        end
+    end)
 end)
